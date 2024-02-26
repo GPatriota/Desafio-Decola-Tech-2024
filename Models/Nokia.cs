25acfda9fecbc9b45240328e4dcbcf18e16eb0d6
Gustavo Patriota <gpspatriota@gmail.com>
2024-02-26 13:13:54 -0300

    Desafio sobre modelos de celulares concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
+using System.Runtime.CompilerServices;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia (string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine ($"Neste momento o aplicativo {nomeApp} está sendo instalado no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone celular1 = new Iphone (numero: "900000000", modelo: "Iphone", imei: "1111111111", memoria: 16);
+Nokia celular2 = new Nokia (numero: "911111111", modelo: "Nokia", imei: "2222222222", memoria: 8);
+
+Console.WriteLine ("TESTE IPHONE");
+Console.WriteLine ($"O número do iphone é {celular1.Numero}");
+celular1.Ligar("Gabriel");
+celular1.ReceberLigacao("Gustavo");
+celular1.InstalarAplicativo("Tiktok");
+
+Console.WriteLine ("");
+
+Console.WriteLine ("TESTE NOKIA");
+Console.WriteLine ($"O número do nokia é {celular2.Numero}");
+celular2.Ligar("Gabriel");
+celular2.ReceberLigacao("Gustavo");
+celular2.InstalarAplicativo("Tiktok");
